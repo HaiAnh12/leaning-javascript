04dacb24f4f38a667f32090ac1cca2472e605bec
HaiAnh12 <91648362+HaiAnh12@users.noreply.github.com>
2023-01-04 11:25:32 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+// Khai bao bien 
+//var fullName = 'Nguyen Hai Anh';
+//var age="20";
+/*
+Noi dung trong day
+*/
+
+// Goi ham thong bao
+//alert(fullName); // Ham de bat len o box
+//alert(age);
+
+// var fullName='Nguyen Hai Anh'
+// console.log(fullName)
+//prompt('Xac nhan ban du tuoi')
+
+//setTimeout(function() {
+//console.log('Day la thong bao')
+//}, 1000)
+function displayDate() {
+    document.getElementById("demo").innerHTML = Date();
+  }
+  
